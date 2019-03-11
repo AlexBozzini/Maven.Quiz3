064b086c4dcd9f2e0890ba177376dbca15b5b833
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-11 10:51:50 -0400

    First edits to enums

--- src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
+++ src/main/java/rocks/zipcode/io/quiz3/objectorientation/enums/RockPaperScissorHandSign.java
@@ -1,10 +1,14 @@
 package rocks.zipcode.io.quiz3.objectorientation.enums;
 
+import java.util.Map;
+
 /**
  * @author leon on 09/12/2018.
  */
 public enum RockPaperScissorHandSign {
-    ADD_ENUMERATIONS_HERE;
+    ROCK,
+    PAPER,
+    SCISSOR;
 
     public RockPaperScissorHandSign getWinner() {
         return null;
