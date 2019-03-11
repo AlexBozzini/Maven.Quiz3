7df20a40139f4c45fef8f6dba5010e8bb3a0b419
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-11 10:12:07 -0400

    Finished square array analyzer

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,10 +1,23 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
     public static Boolean compare(Integer[] input, Integer[] squaredValues) {
-        return null;
+        List<Integer> inputList = new ArrayList<>(Arrays.asList(input));
+        List<Integer> squaredValuesList = new ArrayList<>(Arrays.asList(squaredValues));
+        for (Integer i : inputList){
+            if (squaredValuesList.contains(i * i)){
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
