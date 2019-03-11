2150c234565150409231ed5386f05371c769f2e2
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-03-11 09:39:01 -0400

    First attempt pig latin generator

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -1,10 +1,46 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.Arrays;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class PigLatinGenerator {
+    private char[] merge(char[] array1, char[] array2) {
+        char[] mergedArray = new char[array1.length + array2.length];
+        System.arraycopy(array1, 0, mergedArray, 0, array1.length);
+        System.arraycopy(array2, 0, mergedArray, array1.length, array2.length);
+        return mergedArray;
+    }
     public String translate(String str) {
-        return null;
+        char[] stringArray = str.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        Integer startingInt = 0;
+        /*if (!str.matches("[a-zA-Z]")){
+            return null;
+        } else if (!str.contains("[a,e,i,o,u,A,E,I,O,U]")){
+            return str + "ay";
+        } else */if (stringArray[0] == 'a' || stringArray[0] == 'e' || stringArray[0] == 'i' ||stringArray[0] == 'o' ||stringArray[0] ==  'u') {
+            for (char c: stringArray) {
+                builder.append(c);
+            }
+            builder.append("way");
+        } else {
+            char[] rotatedArray = new char[stringArray.length];
+            for(int i = 0; i < stringArray.length; i++){
+                if (stringArray[i] == 'a' || stringArray[i] == 'e' || stringArray[i] == 'i' ||stringArray[i] == 'o' ||stringArray[i] ==  'u'){
+                    startingInt = i;
+                    break;
+                }
+            }
+            char[] arrayFirstHalf = Arrays.copyOfRange(stringArray, startingInt, stringArray.length);
+            char[] arraySecondHalf = Arrays.copyOfRange(stringArray, 0, startingInt);
+            rotatedArray = merge(arrayFirstHalf, arraySecondHalf);
+            for (char c : rotatedArray){
+                builder.append(c);
+            }
+            builder.append("ay");
+        }
+        return builder.toString();
     }
 }
